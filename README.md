e34de2274f6d1a416f1997d36dd5b6347d3a8a32
Alambek Gulamidinov <alan@Alambeks-MacBook-Air.local>
2023-06-05 20:59:48 +0900

    Saved local changes to git

---
+++ README.md
@@ -0,0 +1,4 @@
+LiveBright
+Calendar application with location-based event discovery App features a customizable calendar view with location-based event discovery and advanced filtering options. Users can filter events by categories such as music, sports, and art, as well as by date, time, and location. The app allows users to save events to their calendar, purchase tickets, and share events with friends. Below is a chart which shows the availability of features in LiveBright and other existing solutions:
+
+By team @2any1
---
+++ app.js
@@ -0,0 +1,104 @@
+const express = require("express")
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'));
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res){
+    res.render("Login", {value:"style=display:none"})
+    MainSearchBox = []
+    MyCSearchBox = []
+    isAddedtoCal = false;
+});
+
+
+app.post('/', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username === 'Test' && password === 'test') {
+        res.redirect('/main');
+    } else {
+        res.render("Login", {value: ""})
+    }   
+})
+
+let MainSearchBox = []
+let MyCSearchBox = []
+let isAddedtoCal = false
+
+app.get("/main", function(req, res){
+    res.render("Main", {newListItems:MainSearchBox})
+    MyCSearchBox = []
+})
+app.post("/main", (req, res) => {
+    console.log(req.body.search)
+    MainSearchBox.push(req.body.search)
+    res.redirect("/main")
+})
+app.post("/mainAdd", (req, res) => {
+    MainSearchBox.push(req.body.name)
+    res.redirect("/main")
+})
+
+app.get("/day", function(req, res){
+    res.render("Day")
+    MyCSearchBox = []
+    MainSearchBox = []
+})
+
+
+
+app.post("/gotoDay", function(req, res){
+    res.redirect("/day")
+})
+app.get('/myC', (req, res) => {
+    MainSearchBox = []
+    res.render("myCalendar", {newListItems: MyCSearchBox, isAdded: isAddedtoCal})
+    //res.sendFile(__dirname + "/myCalendar.html")
+})
+app.post('/myC', (req, res) => {
+    MyCSearchBox.push(req.body.search)
+    res.redirect('/myC')
+})
+
+app.get('/add', (req, res) => {
+    res.render("addEvent")
+    MainSearchBox = []
+    MyCSearchBox = []
+    //res.sendFile(__dirname + "/addEvent.html")
+})
+app.get('/explore', (req, res) => {
+    res.render("explore")
+    MainSearchBox = []
+    MyCSearchBox = []
+    //res.sendFile(__dirname + "/explore.html")
+})
+app.get('/event', (req, res) => {
+    res.render('Event', {isAdded: isAddedtoCal})
+})
+app.post('/event', (req, res) => {
+    isAddedtoCal = true;
+    res.redirect('/event');
+})
+
+
+app.post('/gotoEvent', (req, res) => {
+    res.redirect('/event')
+})
+app.post('/gotoMyC', (req, res) => {
+    res.redirect("/myC")
+})
+app.post('/gotoHome', (req, res) => {
+    res.redirect("/main")
+})
+app.post('/gotoAdd', (req, res) => {
+    res.redirect("/add")
+})
+app.post('/gotoExplore', (req, res) => {
+    res.redirect("/explore")
+})
+
+app.listen(8888, ()=>{
+    console.log("Server is running")
+})
