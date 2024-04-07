d9ca69ba1c965c875e3d5c1c47acb46384ccd0fd
Alambek Gulamidinov <alan@Alambeks-MacBook-Air.local>
2024-04-07 20:32:01 +0900

    connect db and implement login&signup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,25 +1,30 @@
 const jwt = require('jsonwebtoken');
-// const User = require('../models/User');
+const pool = require('../db');
+// const bcrypt = require('bcrypt');
 
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
   console.log(email, password)
-
-  try {
-    console.log("Login is here")
-    // const user = await User.findOne({ email });
-    // if (!user) {
-    //   return res.status(404).send('User not found');
+  try{
+    const { rows } = await pool.query(`select * from users where email = $1`, [email]);
+    const user = rows[0];
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    console.log(password, user.password)
+    // const isMatch = await bcrypt.compare(password, user.password);
+    // if (!isMatch) {
+    //   return res.status(401).json({ message: 'Invalid credentials' });
     // }
-
-    // if (password !== user.password) {
-    //   return res.status(401).send('Invalid password');
-    // }
-
-    // const token = jwt.sign({ userId: user._id }, process.env.SECRET, { expiresIn: '24h' });
-
-    res.status(200).send('OK');
-  } catch (error) {
+    if(password === user.password) {
+      res.status(200).send('OK');
+    }
+    else{
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+  }
+  catch(err){
+    console.log(err);
     res.status(500).send('Error logging in user');
   }
 };
@@ -29,8 +34,11 @@ exports.signUpUser = async (req, res) => {
   console.log(email, password)
 
   try {
-    console.log("Sign-Up is here")
-    
+    console.log("Sign-Up")
+    await pool.query(
+      "INSERT INTO users (email, password) VALUES ($1, $2)",
+      [email, password]
+    );
     res.status(200).send('OK');
   } catch (error) {
     res.status(500).send('Error logging in user');
---
+++ backend/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error connecting to the database', err.stack);
+  } else {
+    console.log('Database connection successful', res.rows[0]);
+  }
+});
+
+module.exports = pool;
